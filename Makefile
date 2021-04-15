3072128b34f56123909bcb619487af00a5f1b371
Zayac <ZayacTheEngineer@protonmail.com>
2021-04-16 02:05:36 +0300

    lmao

---
+++ Makefile
@@ -0,0 +1,8 @@
+all: turnip.o
+	gcc turnip.o -o turnip
+turnip.o:
+	gcc -c turnip.c -o turnip.o
+clean:
+	rm turnip.o
+	rm turnip
+
---
+++ turnip.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+	puts(
+	"                         ;;WW,,\n"
+	"                        zzWWzz\n"
+	"                       XXzzUU          ..XX..\n"
+	"                       CC++UU..      ,,CCCC\n"
+	"      ..,,            MMzzXX++    ,,UUCCXX\n"
+	"       ;;WWUUzz      ++XXCC++UU  ..XXzzWW++\n"
+	"        MMzzUUUU,,  UUzzUU  UU  XX++UUUU,,\n"
+	"        UUMMMM;;UU;;UUUUXX++MMzzzzMMzzUU....++..\n"
+	"         ,,WWXXMMUUCCzzCCUUXXXXCCXXWWCCXXzzMMWW,,\n"
+	"           XXUU;;UUXXzzUUMMXX++UUUUXXMMMMUUUU++\n"
+	"             XXMMzzCCXXzzWW;;  CCUUXXCC;;UUzz\n"
+	"               ++UUWWWWXXXX++,,WWUUMMXXXX,,\n"
+	"                   ,,XXMMCCCCUUWWUU,,..\n"
+	"                       UUzzWWWW;;\n"
+	"                       ++UUMMUU..\n"
+	"                     XXWWWWWWMMzz\n"
+	"                   ;;CCWWWWWWWWCCUU++\n"
+	"                   UUWWWWWWWWWWWWXXUU\n"
+	"                  ++WWWWWWWWWWWWMMMMMM..\n"
+	"                  UUUUWWWWWWWWWWWWCCMM,,\n"
+	"                  UUCCWWWWWWWWWWCC,,CC\n"
+	"                  XXCCMMWWWWWWWWUUzzUU\n"
+	"                  ,,UU;;MMWWWWWW++CC,,\n"
+	"                   zzUUUUMMWWMMXXXX\n"
+	"                    zzMMCCMMUUCC..\n"
+	"                      ..MMMMMM;;\n"
+	"                        ,,WWXX\n"
+	"                           CC,,\n"
+	);
+}
